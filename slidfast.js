d5ddd6316a587d160e997d9b97f3718b1ebd7567
Wesley Hales <wesleyhales@gmail.com>
2012-05-24 14:09:22 -0400

    better orientation based navigation

--- slidfast.js
+++ slidfast.js
@@ -25,7 +25,7 @@
 
             options,
 
-            defaultPageID = "",
+            defaultPageID = null,
 
             touchEnabled = false,
 
@@ -304,7 +304,7 @@
 
       slidfast.ui = slidfast.prototype = {
 
-         slideTo : function(id) {
+         slideTo : function(id,callback) {
             if (!focusPage) {
                focusPage = getElement(defaultPageID);
             }
@@ -364,6 +364,13 @@
             if (touchEnabled) {
                new slidfast.ui.Touch(focusPage);
             }
+
+            if(callback){
+                //time of transition - todo convert css to javascript here
+                //we're creating a way to have a callback at the end of the transition/page slide
+                setTimeout(callback,500);
+            }
+
 
          },
 
@@ -689,51 +696,66 @@
 
          init : function() {
             if (slidfast.html5e.supports_orientation) {
-               window.addEventListener("deviceorientation", function(event) {
-                  //alpha: rotation around z-axis
-                  var rotateDegrees = event.alpha;
-                  //gamma: left to right
-                  var leftToRight = event.gamma;
-                  //beta: front back motion
-                  var frontToBack = event.beta;
-
-                  handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-               }, false);
+                if(!focusPage){
+                   focusPage = getElement(defaultPageID)
+                }
+                slidfast.orientation.nav();
             }
+         },
 
-            var leftcount = 0;
-            var rightcount = 0;
+         nav : function () {
+
+           window.addEventListener("deviceorientation", function(event) {
+              //alpha: rotation around z-axis
+              var rotateDegrees = event.alpha;
+              //gamma: left to right
+              var leftToRight = event.gamma;
+              //beta: front back motion
+              var frontToBack = event.beta;
+
+              handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+           }, false);
+
+
+            var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+                //on each movement, we're controlling how the current focusPage moves
+                var curTransform = new WebKitCSSMatrix(window.getComputedStyle(focusPage).webkitTransform);
+                focusPage.innerHTML = leftToRight;
+                focusPage.style.webkitTransform = 'translate3d(' + leftToRight * 5 + 'px, 0, 0)';
+                focusPage.style.WebkitTransition = 'all .5s ease-out';
+                navigate(leftToRight)
+            };
+
+            var leftcount = 0,rightcount = 0;
             var leftpages = listToArray(document.querySelectorAll('.page'));
-
             var rightpages = leftpages.slice();
             var leftPageCount = leftpages.length;
             var rightPageCount = rightpages.length;
-            var delay, leftTimeout, rightTimeout;
-            var pagehistory = [];
-            var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
-               //yo - this needs refactoring for reuse and object orientation...
-               if (leftToRight > 45) {
+            var delay, keepgoing = true, pagehistory = [];
+
+            //yo - this needs refactoring for reuse (left and rich conditionals are dupd) and object orientation...
+            function navigate(leftToRight){
+                //boolean so we don't do all the slides at once.
+                if(keepgoing){
+                //decide which way the phone is tilted
+                if (leftToRight > 30) {
 
                   if (leftcount <= (leftPageCount + 1)) {
+                      //reset
                      if (leftcount == 0) {
-                        clearTimeout(rightTimeout);
                         if (pagehistory.length > 0) {
-
                            leftpages = pagehistory;
                            pagehistory = [];
                         }
                         leftcount++
                      } else {
                         var leftpage = leftpages.pop();
-                        //pop off the stack before queue
                         if (leftpage != undefined) {
+                           leftpage.className = 'page stage-left';
                            pagehistory.push(leftpage);
                            delay = leftcount * 500;
                            leftcount++;
-                           leftTimeout = setTimeout(function() {
-                              slidfast.ui.slideTo(leftpage);
-                           }, delay);
-
+                           slideQueue(leftpage);
                         } else {
                            rightcount = 0;
                         }
@@ -744,7 +766,6 @@
                   if (rightcount <= (rightPageCount + 1)) {
                      //reset
                      if (rightcount == 0) {
-                        clearTimeout(leftTimeout);
                         if (pagehistory.length > 0) {
                            rightpages = pagehistory;
                            pagehistory = [];
@@ -753,38 +774,46 @@
                      } else {
                         var rightpage = rightpages.pop();
                         if (rightpage != undefined) {
+                           rightpage.className = 'page stage-right';
                            pagehistory.push(rightpage);
                            delay = rightcount * 500;
                            rightcount++;
-                           rightTimeout = setTimeout(function() {
-                              slidfast.ui.slideTo(rightpage);
-                           }, delay);
+                           slideQueue(rightpage);
                         } else {
                            leftcount = 0;
                         }
                      }
                   }
-
-               }
-            };
-
-
-            if (slidfast.html5e.supports_motion) {
-               window.addEventListener('devicemotion', deviceMotionHandler, false);
+                }
+            }
             }
 
-            function deviceMotionHandler(eventData) {
+            function slideQueue(page){
+                keepgoing = false;
+                //simple way to put a block on the calling code. Since the orientation is a constant change
+                slidfast.ui.slideTo(page, function(){keepgoing = true});
+            }
+
+         },
+
+         motion : function() {
+
+           if (slidfast.html5e.supports_motion) {
+               window.addEventListener('devicemotion', deviceMotionHandler, false);
+           }
+
+           function deviceMotionHandler(eventData) {
                // Grab the acceleration including gravity from the results
                var acceleration = eventData.accelerationIncludingGravity;
 
                // Display the raw acceleration data
                var rawAcceleration = "[x " + Math.round(acceleration.x) + ", y " +
-                     Math.round(acceleration.y) + ", z " + Math.round(acceleration.z) + "]";
+                   Math.round(acceleration.y) + ", z " + Math.round(acceleration.z) + "]";
 
                // Z is the acceleration in the Z axis, and if the device is facing up or down
                var facingUp = -1;
                if (acceleration.z > 0) {
-                  facingUp = +1;
+                   facingUp = +1;
                }
 
                // Convert the value from acceleration to degrees acceleration.x|y is the
@@ -797,15 +826,91 @@
 
                // Apply the 2D rotation and 3D rotation to the image
                var rotation = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB) + "deg)";
-               document.getElementById("home-page").style.webkitTransform = rotation;
-            }
-
+               focusPage.style.webkitTransform = rotation;
+           }
          }
       };
 
       slidfast.worker = slidfast.prototype = {
-         //
-         init : function(workerurl) {
+          //
+          init:function (script,pool,callback) {
+
+              function Pool(size) {
+                  var _this = this;
+
+                  // set some defaults
+                  this.taskQueue = [];
+                  this.workerQueue = [];
+                  this.poolSize = size;
+
+                  this.addWorkerTask = function (workerTask) {
+                      if (_this.workerQueue.length > 0) {
+                          // get the worker from the front of the queue
+                          var workerThread = _this.workerQueue.shift();
+                          workerThread.run(workerTask);
+                      } else {
+                          // no free workers,
+                          _this.taskQueue.push(workerTask);
+                      }
+                  };
+
+                  this.init = function () {
+                      // create 'size' number of worker threads
+                      for (var i = 0; i < size; i++) {
+                          _this.workerQueue.push(new WorkerThread(_this));
+                      }
+                  };
+
+                  this.freeWorkerThread = function (workerThread) {
+                      if (_this.taskQueue.length > 0) {
+                          // don't put back in queue, but execute next task
+                          var workerTask = _this.taskQueue.shift();
+                          workerThread.run(workerTask);
+                      } else {
+                          _this.taskQueue.push(workerThread);
+                      }
+                  }
+              }
+
+              // runner work tasks in the pool
+              function WorkerThread(parentPool) {
+
+                  var _this = this;
+
+                  this.parentPool = parentPool;
+                  this.workerTask = {};
+
+                  this.run = function (workerTask) {
+                      this.workerTask = workerTask;
+                      // create a new web worker
+                      if (this.workerTask.script != null) {
+                          var worker = new Worker(workerTask.script);
+                          worker.addEventListener('message', dummyCallback, false);
+                          worker.postMessage(workerTask.startMessage);
+                      }
+                  };
+
+                  // for now assume we only get a single callback from a worker
+                  // which also indicates the end of this worker.
+                  function dummyCallback(event) {
+                      // pass to original callback
+                      _this.workerTask.callback(event);
+
+                      // we should use a seperate thread to add the worker
+                      _this.parentPool.freeWorkerThread(_this);
+                  }
+
+              }
+
+              // task to run
+              function WorkerTask(script, callback, msg) {
+                  this.script = script;
+                  this.callback = callback;
+                  this.startMessage = msg;
+              }
+
+              //---------------------------------------------
+
             function log(msg) {
                // Use a fragment: browser will only render/reflow once.
                var fragment = document.createDocumentFragment();
@@ -815,10 +920,10 @@
                document.querySelector("#log").appendChild(fragment);
             }
 
-               var worker = new Worker(workerurl);
+               var worker = new Worker(script);
                worker.onmessage = function(e) {
-                  log("Received: " + e.data);
-               }
+                  //log("Received: " + e.data);
+               };
                worker.postMessage(); // Start the worker.
 
             }
